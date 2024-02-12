ffc4a895a6dc9858e41071723749a027274d3c3b
Romi1804000 <Romiomer145@gmail.com>
2024-02-12 16:56:06 +0200

    OMG

--- index.h
+++ index.h
@@ -2,10 +2,50 @@
 #define INDEX_H
 
 #include "trie/trie.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-void print_string(void *word_ctx, void *do_ctx);
+#define SPACES "\f\v\t "
+
+struct trie_ctx {
+    const char *string;
+    int *lines_index;
+    int line_index_counter;
+    int capacity;
+};
+
+/**
+ * Duplicate a string using dynamic memory allocation.
+ *
+ * @param s The string to duplicate.
+ * @return A pointer to the newly allocated string.
+ */
+char *my_strdup(const char *s);
+
+/**
+ * Extracts the next word from a string, given a set of delimiter characters.
+ *
+ * @param current The current position in the string.
+ * @param next Pointer to the next word position in the string.
+ * @return The next word extracted from the string.
+ */
 char *get_next_word(char *current, char **next);
-void trie_free(Trie trie);
 
-#endif /* INDEX_H */
+/**
+ * Prints the occurrences of a word along with the line numbers it appears on.
+ *
+ * @param word_ctx Context containing word and its occurrences.
+ * @param do_ctx Additional context (unused in this function).
+ */
+void print_string(void *word_ctx, void *do_ctx);
+
+/**
+ * Cleans up dynamically allocated memory for a trie context.
+ *
+ * @param word_ctx The trie context to clean up.
+ * @param do_ctx Additional context (unused in this function).
+ */
+void trie_ctx_cleanup(void *word_ctx, void *do_ctx);
+
+#endif // INDEX_H
 
